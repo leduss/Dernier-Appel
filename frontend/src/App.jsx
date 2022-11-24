081717749b740c761f5a584c8b299aed708a2beb
NicolasBaudet <baudetnicolas@hotmail.fr>
2022-11-24 16:27:02 +0100

    first commit on card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-
 import "./App.css";
+// eslint-disable-next-line import/no-unresolved
+import Chrono from "@components/Chrono";
 
 function App() {
   return (
     <div className="App">
       <p>coucou</p>
+      <Chrono />
     </div>
   );
 }
