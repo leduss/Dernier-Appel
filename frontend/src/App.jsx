23ec085e47397d4aaefa0a35ce1c0aa769bf6c54
paul battault <paulbattault@gmail.com>
2022-11-24 16:57:25 +0100

    router done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router } from "react-router-dom";
 import Chrono from "./components/Chrono/Chrono";
+import Transition from "./Transition";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Chrono />
+      <Router>
+        <Chrono />
+        <Transition />
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/">
+        <button type="button">home</button>
+      </Link>
+      <Link to="/planets/tatooine">
+        <button type="button">planete1</button>
+      </Link>
+      <Link to="/planets/pandora">
+        <button type="button">planete2</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
